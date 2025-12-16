97f40ee95d12ba3135198fc1102d43bfeefc12a7
gagedorl <gagedorlando@gmail.com>
2025-12-15 18:05:13 -0700

    Fixed user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,22 +4,6 @@ import { getUsers, getUserByUsername, createUser, updateUser, deleteUser } from 
 import pkg from 'express-openid-connect';
 const {auth, requiresAuth} = pkg;
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.AUTH0_SECRET,
-    baseURL: process.env.AUTH0_BASE_URL,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    routes: {
-        login: '/authorize',
-        callback: '/callback',
-        logout: '/logout'
-    }
-}
-
-router.use(auth(config));
-
 router.get('/', getUsers);
 router.get('/:username', getUserByUsername);
 router.post('/', createUser);
